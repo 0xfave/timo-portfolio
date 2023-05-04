d2ce0bd3b568dfa2bf4cbc1a55f5994d612fd2b8
Favour Adetona <96779581+0xfave@users.noreply.github.com>
2023-05-03 21:57:42 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-    <script async type="text/javascript" src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=Wagf62"></script>
       <body>
         <Main />
         <NextScript />
